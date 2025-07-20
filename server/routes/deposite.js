5bb1acecbf080b16712ddfba2d6c20c4076444ea
danaasamuel2023 <danaasamuel20frimpong@gmail.com>
2025-07-20 11:05:50 -1200

    api key

--- server/routes/deposite.js
+++ server/routes/deposite.js
@@ -6,7 +6,7 @@ const { User, Transaction } = require("../schema/schema");
 dotenv.config();
 const router = express.Router();
 
-const PAYSTACK_SECRET_KEY = 'sk_live_0fba72fb9c4fc71200d2e0cdbb4f2b37c1de396c';
+const PAYSTACK_SECRET_KEY = 'sk_live_b8f78b58b7860fd9795eb376a8602eba072d6e15';
 
 if (!PAYSTACK_SECRET_KEY) {
   throw new Error("Paystack secret key is missing in environment variables");
